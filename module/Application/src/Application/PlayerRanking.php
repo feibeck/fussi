644bebfca6aa38f714e537befe2cecc1746c7c54
Steven Klar <steven.klar@mayflower.de>
2012-12-13 11:00:43 +0100

    Add some comments

--- module/Application/src/Application/PlayerRanking.php
+++ module/Application/src/Application/PlayerRanking.php
@@ -12,38 +12,71 @@ class PlayerRanking
      */
     protected $player;
 
+    /**
+     * @var Score
+     */
     protected $score = 0;
 
+    /**
+     * @var Goals
+     */
     protected $goals = 0;
 
+    /**
+     * @var Matches
+     */
     protected $matches = 0;
 
+    /**
+     * @param  Player $player
+     * 
+     * @return void
+     */
     public function __construct(Player $player)
     {
         $this->player = $player;
     }
 
+    /**
+     * @param int $goals
+     * 
+     * @return void
+     */
     public function addGoals($goals)
     {
         $this->goals += $goals;
     }
 
+    /**
+     * @param int $points
+     * 
+     * @return void
+     */
     public function addPoints($points)
     {
         $this->matches++;
         $this->score += $points;
     }
 
+    /**
+     * @return int
+     */
     public function getScore()
     {
         return $this->score;
     }
 
+    /**
+     * @return Player
+     */
     public function getPlayer()
     {
         return $this->player;
     }
 
+    /**
+     * @return int
+     */
     public function getMatchCount()
     {
         return $this->matches;
