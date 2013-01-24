36c300a162dcb1cbe6b639241c2f44c7bf736f75
Florian Eibeck <florian.eibeck@mayflower.de>
2013-01-24 19:46:20 +0100

    Fixed query

--- module/Application/src/Application/Entity/MatchRepository.php
+++ module/Application/src/Application/Entity/MatchRepository.php
@@ -52,7 +52,7 @@ class MatchRepository extends EntityRepository
         $end->modify('last day of');
 
         $query = $this->_em->createQuery(
-            'SELECT m FROM Application\Entity\Match m
+            'SELECT m FROM Application\Entity\SingleMatch m
             WHERE m.date >= :start AND m.date <= :end
             AND m.player1 = :player1 AND m.player2 = :player2
             AND m.tournament = :tournament'
