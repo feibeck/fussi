bfc19a98d3c4d6d5607d8588a9a8d004c46ec354
Martin Brotzeller <brotzeller@mayflower.de>
2013-02-15 16:54:44 +0100

    dropdown for tournaments in menu

--- module/Application/config/view-helper.config.php
+++ module/Application/config/view-helper.config.php
@@ -1,14 +1,23 @@
 <?php
 
 use Application\ViewHelper\Ranking;
+use Application\ViewHelper\Tournament;
 use Doctrine\ORM\EntityManager;
 use Zend\View\HelperPluginManager;
+
 
 return array(
     'invokables' => array(
        'match' => 'Application\ViewHelper\Match',
     ),
     'factories' => array(
+        'tournamentList' => function(HelperPluginManager $manager) {
+            /** @var $em EntityManager */
+            $em = $manager->getServiceLocator()
+                      ->get("doctrine.entitymanager.orm_default");
+            $repo = $em->getRepository('Application\Entity\Tournament');
+            return new Tournament($repo);
+        },
         'ranking' => function(HelperPluginManager $manager) {
             /** @var $em EntityManager */
             $em = $manager->getServiceLocator()
