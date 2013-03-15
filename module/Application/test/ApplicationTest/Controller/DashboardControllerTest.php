b05d0a6078ee858091f3768bc99ecdf1534990f1
Florian Eibeck <florian.eibeck@mayflower.de>
2013-03-15 16:47:17 +0100

    Testing the dashboard controller 

---
+++ module/Application/test/ApplicationTest/Controller/DashboardControllerTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Definition of ApplicationTest\Controller\DashboardControllerTest
+ *
+ * @copyright Copyright (c) 2013 The Fußi-Team
+ * @license   THE BEER-WARE LICENSE (Revision 42)
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * The Fußi-Team wrote this software. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy us a beer in return.
+ */
+
+namespace ApplicationTest\Conroller;
+
+use Application\Controller\DashboardController;
+
+class DashboardControllerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testDashboard()
+    {
+
+        $matchRepository = $this->getMock(
+            'Application\Model\Repository\MatchRepository',
+            array(
+                'getLastMatches'
+            ),
+            array(),
+            '',
+            false
+        );
+        $matchRepository->expects($this->once())
+            ->method('getLastMatches')
+            ->will($this->returnValue('lastmatches'));
+
+        $tournamentRepository = $this->getMock(
+            'Application\Model\Repository\TournamentRepository',
+            array(),
+            array(),
+            '',
+            false
+        );
+        $tournamentRepository->expects($this->once())
+            ->method('findAll')
+            ->will($this->returnValue('alltournaments'));
+
+        $controller = new DashboardController(
+            $matchRepository,
+            $tournamentRepository
+        );
+
+        $viewModel = $controller->dashboardAction();
+
+        $this->assertEquals(
+            array('matches' => 'lastmatches', 'tournaments' => 'alltournaments'),
+            $viewModel
+        );
+    }
+
+}
+
