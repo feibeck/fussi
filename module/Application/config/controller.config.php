4c67c225589d942b0e924d2e520814bbb53b8b39
Steven Klar <steven.klar@mayflower.de>
2014-01-28 12:32:27 +0100

    Create MatchFeeder which should handle match feeds and add it to DI for controller.

--- module/Application/config/controller.config.php
+++ module/Application/config/controller.config.php
@@ -74,5 +74,18 @@ return array(
                 $em->getRepository('Application\Model\Entity\PointLog')
             );
         },
+        'Application\Controller\Feed' => function(ControllerManager $cm) {
+            $sm = $cm->getServiceLocator();
+            $em = $sm->get('doctrine.entitymanager.orm_default');
+
+            $matchFeeder = new \Application\Model\MatchFeeder(
+                new \Zend\Feed\Writer\Feed,
+                $em->getRepository('Application\Model\Entity\Match')
+            );
+            $matchFeeder->setTitle('Fussi: Recent matches');
+            $matchFeeder->setUrl('http://localhost:8001');
+
+            return new Controller\FeedController($matchFeeder);
+        },
     )
 );
---
+++ module/Application/src/Application/Model/Exceptions/UrlNotFoundException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Definition of Application\Controller\DashboardController
+ *
+ * @copyright Copyright (c) 2014 The Fußi-Team
+ * @license   THE BEER-WARE LICENSE (Revision 42)
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * The Fußi-Team wrote this software. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy us a beer in return.
+ */
+
+namespace Application\Model\Exceptions;
+
+class UrlNotFoundException extends \Exception
+{
+
+}
