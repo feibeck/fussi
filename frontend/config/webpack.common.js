285cf05b8edd68131b1239bd3e52b16a5f720c5b
Florian Eibeck <florian.eibeck@mayflower.de>
2017-02-10 17:10:13 +0100

    Change page title to Fußi

--- frontend/config/webpack.common.js
+++ frontend/config/webpack.common.js
@@ -27,7 +27,6 @@ const ngcWebpack = require('ngc-webpack');
 const HMR = helpers.hasProcessFlag('hot');
 const AOT = helpers.hasNpmFlag('aot');
 const METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
