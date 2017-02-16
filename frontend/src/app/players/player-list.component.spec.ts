036844bd56144277711cf0a9a39d3f5a02bd2fbd
Florian Eibeck <florian.eibeck@mayflower.de>
2017-02-16 21:41:55 +0100

    Added test for the PlayerListComponent

---
+++ frontend/src/app/players/player-list.component.spec.ts
@@ -0,0 +1,97 @@
+import { Component } from '@angular/core';
+import { async, TestBed, ComponentFixture, inject, fakeAsync, tick } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+import { PlayerService } from './player.service';
+import { Player } from './player.model';
+import { PlayerListComponent } from './player-list.component';
+import { PlayerLoadError } from './player-load-error.model';
+
+const playerList = [new Player(1, 'Foo', 0, 0), new Player(2, 'Bar', 0, 0)];
+
+/* tslint:disable:max-classes-per-file */
+/* Don't want to have the stubs in extra files… */
+
+class PlayerServiceStub {
+    public getPlayerList() {
+        // to be spyed on
+    }
+}
+
+@Component({
+    selector: 'dummy',
+    template: ''
+})
+class DummyComponent {
+}
+
+describe('PlayerListComponent', () => {
+
+    let fixture: ComponentFixture<PlayerListComponent>;
+    let component: PlayerListComponent;
+
+    beforeEach(async(() => {
+
+        TestBed.configureTestingModule({
+            declarations: [
+                PlayerListComponent,
+                DummyComponent
+            ],
+            imports: [
+                RouterTestingModule.withRoutes([
+                    { path: 'player/show/:id', component: DummyComponent },
+                    { path: 'player/edit/:id', component: DummyComponent },
+                    { path: 'player/new', component: DummyComponent },
+                ])
+            ],
+            providers: [
+                {
+                    provide: PlayerService,
+                    useClass: PlayerServiceStub
+                }
+            ]
+        }).compileComponents();
+
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PlayerListComponent);
+        component = fixture.componentInstance;
+    });
+
+    it('should be readly initialized', () => {
+        expect(fixture).toBeDefined();
+        expect(component).toBeDefined();
+    });
+
+    it('should load players',
+        fakeAsync(inject([PlayerService], (playerService: PlayerService) => {
+
+        spyOn(playerService, 'getPlayerList').and.returnValue(Observable.of(playerList));
+
+        fixture.detectChanges();
+        tick();
+
+        expect(playerService.getPlayerList).toHaveBeenCalled();
+        expect(component.players).toBe(playerList);
+
+    })));
+
+    it('should handle errors',
+        fakeAsync(inject([PlayerService], (playerService: PlayerService) => {
+
+        spyOn(playerService, 'getPlayerList').and.returnValue(
+            Observable.throw(PlayerLoadError.createGeneralError('foo'))
+        );
+
+        fixture.detectChanges();
+        tick();
+
+        expect(playerService.getPlayerList).toHaveBeenCalled();
+        expect(component.players).toEqual([]);
+        expect(component.error).toBeTruthy();
+        expect(component.errorMessage).toBe('foo');
+
+    })));
+
+});
