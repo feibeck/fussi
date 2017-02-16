35e19d47e01e52b3876ef0d55b266646d8f9a6aa
Florian Eibeck <florian.eibeck@mayflower.de>
2017-02-16 12:52:35 +0100

    Add tests for PlayerService.create method

--- frontend/src/app/players/player.service.spec.ts
+++ frontend/src/app/players/player.service.spec.ts
@@ -276,6 +276,98 @@ describe('PlayerService', () => {
 
     });
 
+    describe('create()', () => {
+
+        const newPlayer = new Player(null, 'Foo', 0, 0);
+
+        it('returns player on success', inject([PlayerService, MockBackend], (playerService, mockBackend) => {
+
+            const mockPlayer: JsonPlayer = {
+                id: 1,
+                name: 'Foo',
+                points: 0,
+                matchCount: 0
+            };
+
+            mockBackend.connections.subscribe((connection) => {
+                connection.mockRespond(new Response(new ResponseOptions({
+                    body: JSON.stringify(mockPlayer)
+                })));
+            });
+
+            playerService.create(newPlayer).subscribe((player: JsonPlayer) => {
+                expect(player.id).toBe(1);
+                expect(player.name).toBe('Foo');
+                expect(player.points).toBe(0);
+                expect(player.matchCount).toBe(0);
+            });
+
+        }));
+
+        it('handles internal server error', inject([PlayerService, MockBackend], (playerService, mockBackend) => {
+
+            mockBackend.connections.subscribe((connection) => {
+                connection.mockError(new Response(new ResponseOptions({
+                    status: 500,
+                    body: 'Internal Server Error'
+                })));
+            });
+
+            playerService.create(newPlayer).subscribe(
+                null,
+                (error: PlayerSaveError) => {
+                    expect(error.isValidationError()).toBeFalsy();
+                    expect(error.getMessage()).toBe(PlayerSaveError.defaultMessage);
+                });
+        }));
+
+        it('handles validation error', inject([PlayerService, MockBackend], (playerService, mockBackend) => {
+
+            const responseErrorBody = {
+                errors: [
+                    {
+                        field: 'name',
+                        message: 'Name already exists'
+                    }
+                ]
+            };
+
+            mockBackend.connections.subscribe((connection) => {
+                connection.mockError(new Response(new ResponseOptions({
+                    status: 422,
+                    body: JSON.stringify(responseErrorBody)
+                })));
+            });
+
+            playerService.create(newPlayer).subscribe(
+                null,
+                (error: PlayerSaveError) => {
+                    expect(error.isValidationError()).toBeTruthy();
+                    expect(error.getValidationMessages()[0].field).toBe('name');
+                    expect(error.getValidationMessages()[0].message).toBe('Name already exists');
+                });
+        }));
+
+        it('handles invalid json in response', inject([PlayerService, MockBackend], (playerService, mockBackend) => {
+
+            mockBackend.connections.subscribe((connection) => {
+                connection.mockRespond(new Response(new ResponseOptions({
+                    status: 200,
+                    body: '{"name":"Foo'
+                })));
+            });
+
+            playerService.create(newPlayer).subscribe(
+                null,
+                (error: PlayerSaveError) => {
+                    expect(error.isValidationError()).toBeFalsy();
+                    expect(error.getMessage()).toBe(PlayerSaveError.defaultMessage);
+                });
+
+        }));
+
+    });
+
     describe('save()', () => {
 
         it('players with an id will be updated', inject([PlayerService], (playerService) => {
