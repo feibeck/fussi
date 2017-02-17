a90384fbd0aeab58cb21dc611f71d6fe7c834025
Florian Eibeck <florian.eibeck@mayflower.de>
2017-02-17 10:32:03 +0100

    Added error handling in ActiveTournamentComponent

--- frontend/src/app/dashboard/active-tournaments/active-tournaments.component.ts
+++ frontend/src/app/dashboard/active-tournaments/active-tournaments.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TournamentService } from '../../../tournament/service/tournament.service';
+import { Tournament } from '../../../tournament/model/tournament.model';
 
 @Component({
     selector: 'active-tournaments',
@@ -7,16 +8,20 @@ import { TournamentService } from '../../../tournament/service/tournament.servic
 })
 export class ActiveTournamentsComponent implements OnInit {
 
-    public tournaments = [];
+    public tournaments: Tournament[] = [];
+
+    public error: boolean = false;
 
     constructor(private tournamentService: TournamentService) {
-
     }
 
     public ngOnInit() {
         this.tournamentService.getActiveTournaments().subscribe(
-            (tournaments) => {
+            (tournaments: Tournament[]) => {
                 this.tournaments = tournaments;
+            },
+            () => {
+                this.error = true;
             }
         );
     }
