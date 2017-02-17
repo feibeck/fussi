b48923c15834b9597a130cb80e3f0a5f8f2a21c6
Florian Eibeck <florian.eibeck@mayflower.de>
2017-02-17 11:33:33 +0100

    Added the list of tournaments to the navigation.
    Needed to fix the navigation bar because the bootstrap nav would require JS code for the collapsible menu items.

--- frontend/src/app/navigation/fussi-navigation.component.ts
+++ frontend/src/app/navigation/fussi-navigation.component.ts
@@ -1,9 +1,35 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ElementRef, HostListener } from '@angular/core';
+import { TournamentService } from '../../tournament/service/tournament.service';
+import { Tournament } from '../../tournament/model/tournament.model';
 
 @Component({
     selector: 'fussi-main-navigation',
     encapsulation: ViewEncapsulation.None,
     templateUrl: './fussi-navigation.component.html'
 })
-export class FussiNavigationComponent {
+export class FussiNavigationComponent implements OnInit {
+
+    public tournaments: Tournament[] = [];
+    public tournamentsOpen = false;
+
+    constructor(private elementRef: ElementRef, private tournamentService: TournamentService) {
+    }
+
+    public ngOnInit(): void {
+        this.tournamentService.getTournaments().subscribe(
+            (tournaments: Tournament[]) => {
+                this.tournaments = tournaments;
+            }
+        );
+    }
+
+    @HostListener('document:click', ['$event'])
+    public onClick(event) {
+        if (!this.elementRef.nativeElement.querySelector('#trnmnts').contains(event.target)) {
+            if (this.tournamentsOpen) {
+                this.tournamentsOpen = false;
+            }
+        }
+    }
+
 }
